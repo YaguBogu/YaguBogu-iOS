294a50a3b91d710910dbda8a12d0f6d041914de8
hemssy <hemsyeons@gmail.com>
2025-11-20 20:32:11 +0900

    [Refactor] #25 - 탭 코디네이터 분리

--- YaguBogu/YaguBogu/Features/Home/View/HomeViewController.swift
+++ YaguBogu/YaguBogu/Features/Home/View/HomeViewController.swift
@@ -14,7 +14,7 @@ class HomeViewController: BaseViewController {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("에러메세지")
     }
     
     override func viewDidLoad() {
