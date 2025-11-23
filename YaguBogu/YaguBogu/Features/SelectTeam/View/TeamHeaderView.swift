970b71c726ccd84a2334239959c945fbc4af28b6
oww10 <dntjr2209@gmail.com>
2025-11-23 17:14:32 +0900

    [Design] 구단 선택화면 오토 레이아웃 수정, font 수정

--- YaguBogu/YaguBogu/Features/SelectTeam/View/TeamHeaderView.swift
+++ YaguBogu/YaguBogu/Features/SelectTeam/View/TeamHeaderView.swift
@@ -6,13 +6,12 @@ class TeamHeaderView: UICollectionReusableView {
     
     let subTitleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont(name: "AppleSDGothicNeo", size: 14)
         
+        label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 14)
         label.numberOfLines = 2
         
         let labelText = "응원하는 구단팀을 선택하고, 구장 날씨를 확인하세요\n한번 선택하신 구장은 변경하실 수 없습니다"
         label.text = labelText
-        
         label.setSpacingInPixels(text: labelText, spacingInPx: 18)
         
         label.textColor = .gray04
@@ -25,7 +24,7 @@ class TeamHeaderView: UICollectionReusableView {
         addSubview(subTitleLabel)
         
         subTitleLabel.snp.makeConstraints { make in
-            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 8, left: 24, bottom: 16, right: 24))
+            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0))
         }
     }
     
