f95e6a0171e85583706c90694601643570cc6f98
zzaeun <leeje57575@gmail.com>
2025-11-25 19:53:26 +0900

    [Feat] #40 - 경기 일정 데이터 모델 구현

--- YaguBogu/YaguBogu/Features/Schedule/Coordinator/ScheduleCoordinator.swift
+++ YaguBogu/YaguBogu/Features/Schedule/Coordinator/ScheduleCoordinator.swift
@@ -1,9 +1,16 @@
 import UIKit
 
 final class ScheduleCoordinator: BaseCoordinator {
-
+    private let team: TeamInfo
+    
+    init(navigationController: UINavigationController, team: TeamInfo) {
+        self.team = team
+        super.init(navigationController: navigationController)
+    }
+    
     override func start() {
-        let viewModel = ScheduleViewModel()
+        super.start()
+        let viewModel = ScheduleViewModel(team: team)
         let viewController = ScheduleViewController(viewModel: viewModel)
 
         navigationController.setViewControllers([viewController], animated: false)
--- YaguBogu/YaguBogu/Features/Schedule/ViewModel/ScheduleViewModel.swift
+++ YaguBogu/YaguBogu/Features/Schedule/ViewModel/ScheduleViewModel.swift
@@ -1,6 +1,29 @@
-import Foundation
+import UIKit
+import RxSwift
+import RxRelay
+import RxCocoa
 
 final class ScheduleViewModel {
+    private let scheduleService: ScheduleService
+    private let disposeBag = DisposeBag()
+    
+    let selectedTeam: BehaviorRelay<TeamInfo>
+    let game = BehaviorRelay<[Game]>(value: [])
 
+
+    init(team: TeamInfo, scheduleService: ScheduleService = ScheduleService()) {
+        self.selectedTeam = BehaviorRelay(value: team)
+        self.scheduleService = scheduleService
+        
+        loadGames(team: team)
+    }
+    
+    private func loadGames(team: TeamInfo) {
+        scheduleService.fetchSchedule(for: team.id, year: 2025)
+            .map { items in
+                items.compactMap { $0.toGame(for: team.id) }
+            }
+            .bind(to: game)
+            .disposed(by: disposeBag)
+    }
 }
-
