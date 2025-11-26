ba845ad71c77a2d7f7a72e3fa6f3654f6cb9a101
hemssy <hemsyeons@gmail.com>
2025-11-26 20:39:41 +0900

    [Design] #55 - weather 배열 + description 추가로 받아오기

--- YaguBogu/YaguBogu/Features/Home/Model/StadiumWeather.swift
+++ YaguBogu/YaguBogu/Features/Home/Model/StadiumWeather.swift
@@ -4,6 +4,7 @@ struct StadiumWeather {
     let temperatureC: Double
     let humidity: Int
     let windSpeed: Double
-    let precipitation: Double?   
+    let precipitation: Double?
+    let description: String
 }
 
